56b4fd43283143f50118312cae21c12fd8c09f99
Jonathan Smith <jonathan.smith722@gmail.com>
2021-10-22 15:58:12 -0700

    Updated 10/22/21

--- JavaScript/Advanced Topics/calculator.js
+++ JavaScript/Advanced Topics/calculator.js
@@ -1,24 +1,74 @@
 var display =document.querySelector("#display")
 var y 
+var hold = 0
+var OP 
+var onOff = 0
 
 function press(x){
     console.log("see me")
-    if(display.innerText >= 0 && display.innerText < 10000000000){
-        y = display.innerText * 10 + x 
-        display.innerText = y
+    if(onOff === 0){
+        if(display.innerText >= 0 && display.innerText < 10000000000){
+            y = display.innerText * 10 + x 
+            display.innerText = y
+        }
+        else{
+            display.innerText = display.innerText
+        }
     }
     else{
-        display.innerText = display.innerText
+        clr()
+        if(display.innerText >= 0 && display.innerText < 10000000000){
+            y = display.innerText * 10 + x 
+            display.innerText = y
+            onOff = 0
+        }
+        else{
+            display.innerText = display.innerText
+        }
     }
+    
 }
 
 function setOP(s){
-    if(s === '/'){}
-    if(s === '*'){}
-    if(s === '-'){}
-    if(s === '+'){}
+    if(s === '/'){
+        hold = display.innerText
+        onOff = 1
+        OP = s
+    }
+    if(s === '*'){
+        hold = display.innerText
+        onOff = 1
+        OP = s
+    }
+    if(s === '-'){
+        hold = display.innerText
+        onOff = 1
+        OP = s
+    }
+    if(s === '+'){
+        hold = display.innerText
+        onOff = 1
+        OP = s
+    }
+}
+
+function calculate(){
+    if(OP === '/'){
+        display.innerText = hold / display.innerText
+    }
+    if(OP === '*'){
+        display.innerText = hold * display.innerText
+    }
+    if(OP === '-'){
+        display.innerText = hold - display.innerText
+    }
+    if(OP === '+'){
+        display.innerText = hold + display.innerText
+        console.log(display.innerText)
+    }
 }
 
 function clr(){
     display.innerText = 0
+    hold = 0
 }
